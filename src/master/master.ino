a34b69d41e9589d3d0be719261d50b58fe5dc873
Tino Hager <tino.hager@nager.at>
2022-07-06 23:43:27 +0200

    optimize module code

--- src/master/master.ino
+++ src/master/master.ino
@@ -1,23 +1,10 @@
-//Libraries for LoRa
+//Libraries for LORA
 #include <SPI.h>
 #include "LoRa.h"
-
-//Libraries for BME280
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
 
 //Libraries for OLED Display SSD1306
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-//Prepare BME280 Sensor
-#define SDA 21
-#define SCL 22
-
-Adafruit_BME280 bme;
-//Prepare a second I2C wire for Sensor
-TwoWire I2Cone = TwoWire(1);
 
 //Prepare OLED Display SSD1306
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -28,24 +15,41 @@ TwoWire I2Cone = TwoWire(1);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
-//Prepare LoRa
+//Prepare LORA
 #define BAND    868E6
 
-double temperature;
-double humidity;
-int counter;
+unsigned int receiveCounter;
 
-void setup() {
-  //Prepare Serial connection
-  Serial.begin(115200);
-  Serial.println("Initialize");
+void displaySmallText(int positionX, int positionY, String text) {
+  display.setTextColor(WHITE);
+  display.setCursor(positionX, positionY);
+  display.setTextSize(1);
+  display.print(text);
+}
 
-  //Reset Display
+void displayNormalText(int positionX, int positionY, String text) {
+  display.setTextColor(WHITE);
+  display.setCursor(positionX, positionY);
+  display.setTextSize(2);
+  display.print(text);
+}
+
+void displayBigText(int positionX, int positionY, String text) {
+  display.setTextColor(WHITE);
+  display.setCursor(positionX, positionY);
+  display.setTextSize(3);
+  display.print(text);
+}
+
+void resetOledDisplay() {
   pinMode(OLED_RST, OUTPUT);
   digitalWrite(OLED_RST, LOW);
   delay(20);
   digitalWrite(OLED_RST, HIGH);
+}
 
+void initializeOledDisplay() {
+  Serial.println(F("Initialize SSD1306 display"));
   //Open connection to Display
   Wire.begin(OLED_SDA, OLED_SCL);
 
@@ -53,87 +57,60 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     while (1);
   }
+}
 
-  //Activate Lora
+void initializeLoRa() {
+  display.clearDisplay();
+  displayNormalText(5, 10, "init");
+  displayNormalText(30, 35, "LoRa");
+  display.display();
+
+  Serial.println(F("Initialize LoRa"));
   SPI.begin(SCK,MISO,MOSI,SS);
   LoRa.setPins(SS,RST_LoRa,DIO0);
   if (!LoRa.begin(BAND))
   {
-    Serial.println("LORA initialization failed!");
+    Serial.println(F("LoRa initialization failed!"));
     while (1);
   }
   LoRa.setSpreadingFactor(12);
   LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
+}
 
-  //Set Pins for BME280 Sensor
-  I2Cone.begin(SDA, SCL, 100000); 
-  
-  if (!bme.begin(0x76, &I2Cone)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
+void setup() {
+  //Prepare Serial connection
+  Serial.begin(115200);
+  Serial.println("Initialize");
+
+  resetOledDisplay();
+  initializeOledDisplay();
+  initializeLoRa();
 }
 
 void loop() {
-  temperature = bme.readTemperature();
-  humidity = bme.readHumidity();
-
-  if (counter % 20 == 0 || counter == 0)
-  {
-    LoRa.beginPacket();
-    LoRa.print("t:" + String(temperature) + "#h:" + String(humidity));
-    LoRa.endPacket();
-  }
-  
-  if (counter == 600)
-  {
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setCursor(5,10);
-    display.setTextSize(2);
-    display.print("I love you");
-  
-    display.setTextColor(WHITE);
-    display.setCursor(30,35);
-    display.setTextSize(3);
-    display.print("Baby");
-  
-    display.display();
-  
-    delay(5000);
-    
-    counter = 0;
-    return;
-  }
-
-  Serial.println("Temperature = " + String(temperature) + "*C");
-  Serial.println("Humidity = " + String(humidity) + "%");
-
   display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.setTextSize(1);
-  display.print("Temperature (degree)");
-
-  display.setTextColor(WHITE);
-  display.setCursor(0,12);
-  display.setTextSize(2);
-  display.print(String(temperature));
-
-  display.setTextColor(WHITE);
-  display.setCursor(0,35);
-  display.setTextSize(1);
-  display.print("Humidity (percentage)");
-
-  display.setTextColor(WHITE);
-  display.setCursor(0,47);
-  display.setTextSize(2);
-  display.print(String(humidity));
   
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    receiveCounter++;
+    displayNormalText(5, 10, "receive " + String(receiveCounter));
+    
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  } else {
+    displayNormalText(5, 10, "wait for");
+    displayNormalText(5, 35, "package...");
+  }
+
   display.display();
-
-  Serial.println();
-
-  counter++;
   delay(1000);
 }
--- src/slave/slave.ino
+++ src/slave/slave.ino
@@ -1,10 +1,23 @@
-//Libraries for LORA
+//Libraries for LoRa
 #include <SPI.h>
 #include "LoRa.h"
+
+//Libraries for BME280
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 //Libraries for OLED Display SSD1306
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+//Prepare BME280 Sensor
+#define SDA 21
+#define SCL 22
+
+Adafruit_BME280 bme;
+//Prepare a second I2C wire for Sensor
+TwoWire I2Cone = TwoWire(1);
 
 //Prepare OLED Display SSD1306
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -15,23 +28,51 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
-//Prepare LORA
+//Prepare LoRa
 #define BAND    868E6
 
-int counter;
+//Identifier
+#define MODULE_IDENTIFIER "SENSOR1"
 
-void setup() {
-  //Prepare Serial connection
-  Serial.begin(115200);
-  Serial.println("Initialize");
+//Sensor config
+#define ROLLING_AVERAGE_DATAPOINT_NUMBER 100
 
-  //Reset Display
+double temperature;
+double averageTemperature;
+double humidity;
+
+unsigned int loopCounter;
+
+void displaySmallText(int positionX, int positionY, String text) {
+  display.setTextColor(WHITE);
+  display.setCursor(positionX, positionY);
+  display.setTextSize(1);
+  display.print(text);
+}
+
+void displayNormalText(int positionX, int positionY, String text) {
+  display.setTextColor(WHITE);
+  display.setCursor(positionX, positionY);
+  display.setTextSize(2);
+  display.print(text);
+}
+
+void displayBigText(int positionX, int positionY, String text) {
+  display.setTextColor(WHITE);
+  display.setCursor(positionX, positionY);
+  display.setTextSize(3);
+  display.print(text);
+}
+
+void resetOledDisplay() {
   pinMode(OLED_RST, OUTPUT);
   digitalWrite(OLED_RST, LOW);
   delay(20);
   digitalWrite(OLED_RST, HIGH);
+}
 
-  Serial.println("Initialize display");
+void initializeOledDisplay() {
+  Serial.println(F("Initialize SSD1306 display"));
   //Open connection to Display
   Wire.begin(OLED_SDA, OLED_SCL);
 
@@ -39,69 +80,79 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     while (1);
   }
-  
+}
+
+void initializeLoRa() {
   display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setCursor(5,10);
-  display.setTextSize(2);
-  display.print("init");
-
-  display.setTextColor(WHITE);
-  display.setCursor(30,35);
-  display.setTextSize(3);
-  display.print("LoRa");
-
+  displayNormalText(5, 10, "Initialize");
+  displayNormalText(5, 35, "LoRa");
   display.display();
 
-  //Activate LoRa
-  Serial.println("Initialize LoRa");
+  Serial.println(F("Initialize LoRa"));
   SPI.begin(SCK,MISO,MOSI,SS);
   LoRa.setPins(SS,RST_LoRa,DIO0);
   if (!LoRa.begin(BAND))
   {
-    Serial.println("LoRa initialization failed!");
+    Serial.println(F("LoRa initialization failed!"));
     while (1);
   }
   LoRa.setSpreadingFactor(12);
+  LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
+}
+
+void initializeTemperatureSensor() {
+  //Set Pins for BME280 Sensor
+  I2Cone.begin(SDA, SCL, 100000); 
+  
+  if (!bme.begin(0x76, &I2Cone)) {
+    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
+    while (1);
+  }
+
+  averageTemperature = bme.readTemperature();
+}
+
+double calculateAverageTemperature (double currentTemperature) {
+  averageTemperature -= averageTemperature / ROLLING_AVERAGE_DATAPOINT_NUMBER;
+  averageTemperature += currentTemperature / ROLLING_AVERAGE_DATAPOINT_NUMBER;
+}
+
+void setup() {
+  //Prepare Serial connection
+  Serial.begin(115200);
+  Serial.println("Initialize");
+
+  resetOledDisplay();
+  initializeOledDisplay();
+  initializeLoRa();
+  initializeTemperatureSensor();
 }
 
 void loop() {
+  temperature = bme.readTemperature();
+  humidity = bme.readHumidity();
+
+  calculateAverageTemperature(temperature);
+
+  if (loopCounter % 20 == 0 || loopCounter == 0)
+  {
+    LoRa.beginPacket();
+    LoRa.print(String(MODULE_IDENTIFIER) + "#t:" + String(temperature) + "#h:" + String(humidity));
+    LoRa.endPacket();
+  }
+
+  Serial.println("Temperature = " + String(temperature) + "*C");
+  Serial.println("Humidity = " + String(humidity) + "%");
 
   display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setCursor(5,10);
-  display.setTextSize(2);
-  display.print("wait for");
-
-  display.setTextColor(WHITE);
-  display.setCursor(5,35);
-  display.setTextSize(2);
-  display.print("package...");
-
+  displaySmallText(0, 0, "Temperature (degree)");
+  displayNormalText(0, 12, String(temperature));
+  displaySmallText(0, 35, "Humidity (percentage)");
+  displayNormalText(0, 47, String(humidity));  
   display.display();
 
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
+  Serial.println();
 
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setCursor(5,10);
-    display.setTextSize(2);
-    display.print("receive");
-    display.display();
-    
-    // received a packet
-    Serial.print("Received packet '");
-
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-
-    delay(1000);
-  }
+  loopCounter++;
+  delay(1000);
 }
